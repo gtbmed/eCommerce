14aca1485314f12ff88e40f5a1c29a52efa51907
gtbmed <jkbarson@gmail.com>
2017-08-27 20:57:32 -0400

    change routing in eCommmerce-controller, add ids to login html

--- controllers/eCommerce-controller.js
+++ controllers/eCommerce-controller.js
@@ -1,9 +1,14 @@
     
 var express = require("express");
+// Requiring path to so we can use relative routes to our HTML files
+var path = require("path");
 
 var router = express.Router();
 
 var db = require("../models");
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
 
